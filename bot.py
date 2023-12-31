f6e747fb4f64c5f7ed15c5a220799022eaec751b
artem <djsak@>
2023-12-31 12:40:09 +0300

    first working but not adjusted

---
+++ bot.py
@@ -0,0 +1,171 @@
+import telebot
+import json
+from info import *
+from telebot import types
+from additional_functions import *
+import random
+
+token = '6797511487:AAET4vGng_8X8bYIHDtocZN_vvQuKIMEnKw'
+
+bot = telebot.TeleBot(token=token)
+
+
+with open('data.json', 'r') as file:
+    try:
+        user_data = json.load(file)
+    except:
+        user_data = {}
+
+
+
+
+
+
+
+@bot.message_handler(commands=['start', 'help'])
+def start(message):
+    user_id = str(message.from_user.id)
+    if message.text == '/start':
+
+        user_data[user_id] = {}
+        random_questions = random.sample(list(questions.keys()), len(questions))
+        user_data[user_id]['all_questions'] = random_questions
+        user_data[user_id]['unpopped_questions'] = random_questions.copy()
+        user_data[user_id]['question_num'] = 0
+        user_data[user_id]['answers'] = {}
+        user_data[user_id]['current_question_key'] = None
+        user_data[user_id]['finished'] = False
+        user_data[user_id]['preferred_language'] = 'rus_version'
+        user_data[user_id]['results'] = None
+
+        markup = markup_func_introduction('rus_version')
+        bot.send_message(message.chat.id, msg_intr_rus_version, reply_markup=markup)
+        with open('data.json', 'w') as file:
+            json.dump(user_data, file)
+    else:
+        language = user_data[user_id]['preferred_language']
+        markup = help_markup(language)
+        text = markup_dict[language]['help_text']
+        bot.send_message(chat_id=user_id, text=text, reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_handler(call):
+    user_id = str(call.message.chat.id)
+
+    if call.data in ['intr_eng_version', 'intr_rus_version', 'help_eng_version', 'help_rus_version']:
+        language = call.data[5:]
+        if call.data.startswith('help'):
+            print('dfkajkdlsajfklsd')
+            text = markup_dict[language]['help_text']
+            markup = help_markup(language)
+        else:
+            text = markup_dict[language]['intr_msg_language']
+            markup = markup_func_introduction(language)
+        user_data[user_id]['preferred_language'] = language
+        bot.edit_message_text(chat_id=user_id, message_id=call.message.message_id,
+                              text=text,
+                              reply_markup=markup)
+
+    elif call.data in ['finish_eng_version', 'finish_rus_version']:
+        language = call.data[7:]
+        redo = False
+        markup = final_markup(language)
+        results = user_data[user_id]['results']
+        bot.edit_message_text(chat_id=user_id, message_id=call.message.message_id,
+                              text=finish_text(language, results), reply_markup=markup)
+
+
+
+    elif call.data in ['rus_version', 'redo_rus_version', 'eng_version',
+                       'redo_eng_version', 'finish_eng_version', 'finish_rus_version']:
+        callback_question_num = int(call.message.text[:2])
+        card_key = user_data[user_id]['all_questions'][len(questions) - callback_question_num]
+        if call.data in ['redo_rus_version', 'redo_eng_version']:
+            language = call.data[5:]
+            redo = True
+            chosen_option_index = user_data[user_id]['answers'][card_key]
+            redo_answer = questions[card_key][call.data[5:]]['options'][chosen_option_index]
+            if user_data[user_id]['question_num'] == len(questions):
+                markup = markup_func_redo(language, final_question=True)
+            else:
+                markup = markup_func_redo(language)
+        else:
+            redo = False
+            redo_answer = False
+            markup = markup_func(call.data)
+            language = call.data
+            if user_data[user_id]['question_num'] == len(questions):
+                markup = markup_func(call.data, final_question=True)
+            else:
+                markup = markup_func(call.data)
+        if card_key == user_data[user_id]['current_question_key']:
+            user_data[user_id]['preferred_language'] = language
+        user_data[user_id]['language_of_question'] = language
+
+        bot.edit_message_text(chat_id=user_id, message_id=call.message.message_id,
+                              text=making_question(language, card_key, callback_question_num,
+                                                   redo, redo_answer),
+                              reply_markup=markup)
+
+    if not user_data[user_id]['finished']:
+        if call.data == 'next_question':
+            if user_data[user_id]['unpopped_questions']:
+                user_data[user_id]['question_num'] += 1
+                user_data[user_id]['current_question_key'] = user_data[user_id]['unpopped_questions'].pop()
+                current_question_key = user_data[user_id]['current_question_key']
+                preferred_language = user_data[user_id]['preferred_language']
+                if user_data[user_id]['question_num'] == len(questions):
+                    markup = markup_func(preferred_language, final_question=True)
+                else:
+                    markup = markup_func(preferred_language)
+                bot.send_message(chat_id=user_id,
+                                 text=making_question(preferred_language, current_question_key, user_data[user_id]['question_num']),
+                                 reply_markup=markup)
+
+        elif call.data in ['0','1','2','3']:
+            callback_question_num = int(call.message.text[:2])
+            card_key = user_data[user_id]['all_questions'][len(questions)-callback_question_num]
+            current_language = 'eng_version' if 'c' in call.message.text[1:4] else 'rus_version'
+
+            if questions[card_key]['eng_version']['options'][int(call.data)] == questions[card_key]['eng_version']['correct_ans']:
+                user_data[user_id]['answers'][card_key] = 1
+            else:
+                user_data[user_id]['answers'][card_key] = 0
+            if callback_question_num == len(questions):
+                markup = markup_func_redo(current_language, final_question=True)
+            else:
+                markup = markup_func_redo(current_language)
+            bot.edit_message_text(chat_id=user_id, message_id=call.message.message_id,
+                                  text=making_question(current_language, card_key, callback_question_num,
+                                  True, questions[card_key][current_language]['options'][int(call.data)]), reply_markup=markup)
+
+        elif call.data in ['redo_callback_eng_version', 'redo_callback_rus_version']:
+            if user_data[user_id]['finished'] == False:
+                callback_question_num = int(call.message.text[:2])
+                card_key = user_data[user_id]['all_questions'][len(questions) - callback_question_num]
+                language = call.data[14:]
+                if callback_question_num == len(questions):
+                    markup = markup_func(language, final_question=True)
+                else:
+                    markup = markup_func(language)
+                bot.edit_message_text(chat_id=user_id, message_id=call.message.message_id,
+                                      text=making_question(call.data[14:], card_key, callback_question_num),
+                                      reply_markup=markup)
+                del user_data[user_id]['answers'][card_key]
+
+        elif call.data in ['finish_callback_rus_version', 'finish_callback_eng_version']:
+            user_data[user_id]['finished'] = True
+            language = call.data[16:]
+            markup = final_markup(language)
+            answers = user_data[user_id]['answers']
+            results = counting_results(answers)
+            user_data[user_id]['results'] = results
+            bot.send_message(chat_id=user_id, text=finish_text(language, results), reply_markup=markup)
+        with open('data.json', 'w') as file:
+            json.dump(user_data, file)
+
+
+
+
+bot.polling()
---
+++ info.py
@@ -0,0 +1,369 @@
+questions = {
+    'ml_1': {
+        'rus_version': {
+            'question': 'Какая главная цель машинного обучения?',
+            'options': [
+                        'Захватить мир🤖🌐🚀',
+                        'Предсказать результат по входным данных📊🔮',
+                        'Генерировать прикольные картинки🖼️😊',
+                        'Научить компьютер думать как человек🤖✨'
+                        ],
+            'correct_ans': 'Предсказать результат по входным данных📊🔮'
+        },
+        'eng_version': {
+            'question': 'What is the primary goal of machine learning?',
+            'options': [
+                        'To conquer the World🤖🌐🚀',
+                        'To predict a result given an input data📊🔮',
+                        'To generate funny images🖼️😊',
+                        "To make computers to think like humans do🤖✨"
+                        ],
+            'correct_ans': 'To predict a result given an input data📊🔮'
+        },
+    },
+    'ml_2': {
+        'rus_version': {
+            'question': 'Какая цель у тренировочного набора данных в машинном обучении?',
+            'options': [
+                        'Оценить работу модели📊👩‍💻',
+                        'Обучить модель путем настройки ее параметров🛠️🤖📚',
+                        'Обучить набор данных для модели📖🤖🔍',
+                        'Набрать тренировку для цели в машинном обучении😵‍💫🌀🤯'
+                        ],
+            'correct_ans': 'Обучить модель путем настройки ее параметров🛠️🤖📚'
+        },
+        'eng_version': {
+            'question': 'What is the purpose of a training set in machine learning?',
+            'options': [
+                        'To evaluate the performance of a model📊👩‍💻',
+                        'To train a model by adjusting its parameters🛠️🤖📚',
+                        'To train a set to feed it into the model📖🤖🔍',
+                        "To set a train for an aim in machine learning😵‍💫🌀🤯"
+                        ],
+            'correct_ans': 'To train a model by adjusting its parameters🛠️🤖📚'
+        },
+    },
+    'ml_3': {
+        'rus_version': {
+            'question': 'Какие основные типы машинного обучения существуют?',
+            'options': [
+                        'Обучение с учителем, обучение без учителя и обучение с подкреплением📚👨‍🏫🔄',
+                        'Распознавание изображений, обработка естественного языка и распознавание речи🖼️📝🗣️',
+                        'Регрессия, классификация и кластеризация📈🏷️🔍',
+                        'Пунктуация, маршрутизация, коллаборация📌🛣️🤝'
+                        ],
+            'correct_ans': 'Обучение с учителем, обучение без учителя и обучение с подкреплением📚👨‍🏫🔄'
+        },
+        'eng_version': {
+            'question': 'What are the main types of machine learning?',
+            'options': [
+                        'Supervised learning, unsupervised learning, and reinforcement learning📚👨‍🏫🔄',
+                        'Image recognition, natural language processing, and speech recognition🖼️📝🗣',
+                        'Regression, classification, and clustering📈🏷️🔍',
+                        "Punctuation, routing, collaboration📌🛣️🤝"
+                        ],
+            'correct_ans': 'Supervised learning, unsupervised learning, and reinforcement learning📚👨‍🏫🔄'
+        }
+    },
+    'ml_4': {
+        'rus_version': {
+            'question': 'Какой алгоритм является базовым для решения задач классификации в машинном обучении',
+            'options': [
+                        'Линейная регрессия📈🔍',
+                        'Бинарный поиск🔍👀🔍',
+                        'Логистическая регрессия📈🔍🧐',
+                        'Алгоритм Евклида🧮📏'
+                        ],
+            'correct_ans': 'Логистическая регрессия📈🔍🧐'
+        },
+        'eng_version': {
+            'question': 'What is the most basic algorithm for classification tasks in machine learning?',
+            'options': [
+                        'Linear regression📈🔍',
+                        'Binary search🔍👀🔍',
+                        'Logistic regression📈🔍🧐',
+                        "Euclid algorithm🧮📏"
+                        ],
+            'correct_ans': 'Logistic regression📈🔍🧐'
+        }
+    },
+    'ml_5': {
+        'rus_version': {
+            'question': 'Какой алгоритм является базовым для задач регрессии в машинном обучении?',
+            'options': [
+                        "Логистическая регрессия📈🔍🧐",
+                        "Линейная регрессия📈🔍",
+                        "Арифметическая прогрессия🔢🔄",
+                        "Великая Депрессия📉😞"
+                        ],
+            'correct_ans': 'Линейная регрессия📈🔍'
+        },
+        'eng_version': {
+            'question': 'Which algorithm is basic for regression tasks in machine learning?',
+            'options': [
+                        'Logistic regression📈🔍🧐',
+                        'Linear regression📈🔍',
+                        'Arithmetic progression🔢🔄',
+                        "Great Depression📉😞"
+                        ],
+            'correct_ans': 'Linear regression📈🔍'
+        }
+    },
+    'dl_6': {
+        'rus_version': {
+            'question': 'Где используются сверточные нейронные сети в своем большинстве?',
+            'options': [
+                        'Обработка изображений🖼️🔄👁️',
+                        'Обработка языка📝🔄👄',
+                        'Обработка звука🔊🔄👂',
+                        "В рыболовном деле🎣🤔"
+            ],
+            'correct_ans': 'Обработка изображений🖼️🔄👁️'
+        },
+        'eng_version': {
+            'question': 'Where convolutional neural networks are used most of all?',
+            'options': [
+                        'Image processing🖼️🔄👁️',
+                        'Language processing📝🔄👄',
+                        'Audio processing🔊🔄👂',
+                        "Fishing business🎣🤔"
+            ],
+            'correct_ans': 'Image processing🖼️🔄👁️'
+        }
+    },
+    'dl_7': {
+        'rus_version': {
+            'question': 'Что лежит в основе архитектуры больших языковых моделей?',
+            'options': [
+                        'Трансформер🤖🔄',
+                        'Мегатрон🤖🚀',
+                        'Чудо✨😲',
+                        "Язык🗣️📚"
+                        ],
+            'correct_ans': 'Трансформер🤖🔄'
+        },
+        'eng_version': {
+            'question': 'What is lying in the basis of large language models?',
+            'options': [
+                'Transformer🤖🔄',
+                'Megatron🤖🚀',
+                'Miracle✨😲',
+                "Language🗣️📚"
+                        ],
+            'correct_ans': 'Transformer🤖🔄'
+        }
+    },
+    'dl_8': {
+        'rus_version': {
+            'question': 'Какая цель использования функции-активации в нейронных сетях?',
+            'options': [
+                'Внести нелинейность в нашу модель🔄📈🤖',
+                'Активировать нашу функцию🚀🔛🧠',
+                'Увеличить переобучение модели📈📈📈🔄🤖',
+                'Уменьшить переобучение модели📉🔄🤖'
+            ],
+            'correct_ans': 'Внести нелинейность в нашу модель🔄📈🤖'
+        },
+        'eng_version': {
+            'question': 'What is the purpose of an activation function in a neural network?',
+            'options': [
+                'To introduce non-linearity to the model🔄📈🤖',
+                'To activate our function🚀🔛🧠',
+                'To increase overfitting of the model📈📈📈🔄🤖',
+                "To decrease overfitting of the model📉🔄🤖"
+                        ],
+            'correct_ans': 'To introduce non-linearity to the model🔄📈🤖'
+        }
+    },
+    'dl_9': {
+        'rus_version': {
+            'question': 'Какая роль у метода обратного распространения ошибки в обучении нейронной сети?',
+            'options': [
+                'Обновление весов нейронной сети на основе функции потери🔄🏋️‍♂️🤖',
+                'Рассылка ошибок нейронной сети всем друзьям📧❌🤖👫',
+                'Регулиризировать нейронную сеть при переобучении📉🔄🤖🔒',
+                'Выдать ответ в соответствии с входными данными📤🤖🔄📥'
+            ],
+            'correct_ans': 'Обновление весов нейронной сети на основе функции потерь🔄🏋️‍♂️🤖'
+        },
+        'eng_version': {
+            'question': 'What is the role of backpropagation in training a neural network?',
+            'options': [
+                'To update weights of neural network based on loss function🔄🏋️‍♂️🤖',
+                'To propagate errors of neural networks straight to our friends',
+                'To regularize neural network when it is overfitted📉🔄🤖🔒',
+                "To output corresponding to the input data📤🤖🔄📥"
+                        ],
+            'correct_ans': 'To update weights of neural network based on loss function🔄🏋️‍♂️🤖'
+        }
+    },
+    'dl_10': {
+        'rus_version': {
+            'question': 'Какова цель рекуррентной нейронной сети (RNN) в глубоком обучении?',
+            'options': [
+                'Классификация изображений🖼️🏷️🤖',
+                'Обработка последовательных данных и улавливание зависимостей🔄📊🤖',
+                'Оптимизация параметров модели📈🔍🔧',
+                'Кластеризация📊🔄🔍'
+            ],
+            'correct_ans': 'Обработка последовательных данных и улавливание зависимостей🔄📊🤖'
+        },
+        'eng_version': {
+            'question': 'What is the purpose of a recurrent neural network (RNN) in deep learning?',
+            'options': [
+                'To classify images🖼️🏷️🤖',
+                'To process sequential data and capture dependencies🔄📊🤖',
+                'To optimize model parameters📈🔍🔧',
+                "To perform clustering📊🔄🔍"
+                        ],
+            'correct_ans': 'To process sequential data and capture dependencies🔄📊🤖'
+        }
+    },
+    'alg_11': {
+        'rus_version': {
+            'question': 'Для чего нужно условное обозначение "О" большое?',
+            'options': [
+                'Выражать удивление, если встречаешь классный алгоритм🤩👾🔮',
+                'Обозначение для описания лучшего сценария скорости выполнения алгоритма📈😄',
+                'бозначение для описания среднего сценария скорости выполнения алгоритма⏰📊🤷‍♂',
+                'Обозначение для описания худшего сценария скорости выполнения алгоритма📉🤔'
+            ],
+            'correct_ans': 'Обозначение для описания худшего сценария скорости выполнения алгоритма📉🤔'
+        },
+        'eng_version': {
+            'question': 'What is a big O notation?',
+            'options': [
+                'To express your wonder when you meet a cool algorithm🤩👾🔮',
+                'Notation for describing best case scenario of the running time of an algorithm📈😄',
+                'Notation for describing average case scenario of the running time of an algorithm⏰📊🤷‍♂',
+                "Notation for describing worst case scenario of the running time of an algorithm📉🤔"
+                        ],
+            'correct_ans': 'Notation for describing worst case scenario of the running time of an algorithm📉🤔'
+        }
+    },
+    'alg_12': {
+        'rus_version': {
+            'question': 'Для чего используется алгоритм Дейкстры?',
+            'options': [
+                'Сортировка элементов в массиве📈🔍🔄',
+                'Поиск элемента в связном списке🔍📝🔗',
+                'Нет такого алгоритма😠👿',
+                'Поиск кратчайшего пути во взвешенном графе🚗🛣️🔍'
+            ],
+            'correct_ans': 'Поиск кратчайшего пути во взвешенном графе🚗🛣️🔍'
+        },
+        'eng_version': {
+            'question': "What is the purpose of Dijkstra's algorithm?",
+            'options': [
+                'Sorting elements in an array📈🔍🔄',
+                'Searching for an element in a linked list🔍📝🔗',
+                "There is no such algorithm, don't confuse me😠👿",
+                "Finding the shortest path in a weighted graph🚗🛣️🔍"
+                        ],
+            'correct_ans': 'Finding the shortest path in a weighted graph🚗🛣️🔍'
+        }
+    },
+    'alg_13': {
+        'rus_version': {
+            'question': 'Какой алгоритм поиска известен своей эффективностью при поиске в отсортированном массиве путем многократного деления интервала поиска пополам?',
+            'options': [
+                'Линейный поиск🔍🔄',
+                'Поиск в глубину🔍🌲🕵️‍',
+                'Бинарный поиск🔍👀🔄',
+                'Поиск в ширину🔍🌊🔄'
+            ],
+            'correct_ans': 'Бинарный поиск🔍👀🔄'
+        },
+        'eng_version': {
+            'question': 'Which searching algorithm is known for its efficiency in searching a sorted array by repeatedly dividing the search interval in half?',
+            'options': [
+                'Linear Search🔍🔄',
+                'Depth-First Search🔍🌲🕵️‍',
+                'Binary Search🔍👀🔄',
+                "Depth-First Search🔍🌊🔄"
+                        ],
+            'correct_ans': 'Binary Search🔍👀🔄'
+        }
+    },
+    'alg_14': {
+        'rus_version': {
+            'question': 'Что такое рекурсия в программировании?',
+            'options': [
+                'Техника решения проблемы путем использования циклов🔁🛠️',
+                'Специальный оператор⚙️🔧',
+                'Термин, означающий вызов функции самой себя🔄📞🤖',
+                'Термин, означающий смену курса в идее построения программы🔄🛣️🔄'
+            ],
+            'correct_ans': 'Термин, означающий вызов фунции самой себя🔄📞🤖'
+        },
+        'eng_version': {
+            'question': 'What is recursion in programming?',
+            'options': [
+                'Technic of solving problems iteratively🔁🛠️',
+                'Special operator⚙️🔧',
+                'A term which means a function that calls itself🔄📞🤖',
+                'A term meaning a change of course in the idea of building a program🔄🛣️🔄'
+                        ],
+            'correct_ans': 'Term which means a function that calls itself🔄📞🤖'
+        }
+    },
+    'alg_15': {
+        'rus_version': {
+            'question': 'Дайте определение термину "алгоритм"',
+            'options': [
+                'Компонент аппаратного обеспечения компьютера🖥️🔧🔍',
+                'Язык программирования🗣️‍💻📝',
+                'Набор действий для решения определенной проблемы🤔📊🔄',
+                'Зашифрованный язык общения с компьютером🔒🤖📡'
+            ],
+            'correct_ans': 'Набор действий для решения определенной проблемы🤔📊🔄'
+        },
+        'eng_version': {
+            'question': 'What is an algorithm by its definition?',
+            'options': [
+                'Component of computer software🖥️🔧🔍',
+                'Programming language🗣️‍💻📝',
+                'Set of actions for solving certain problem🤔📊🔄',
+                "Encrypted language for communication with computer🔒🤖📡"
+                        ],
+            'correct_ans': 'Set of actions for solving certain problem🤔📊🔄'
+        }
+    },
+}
+
+
+
+
+
+msg_intr_rus_version = '''🤖Добро пожаловать машинно-обученческую бот-анкету!🧠🤖
+
+Приготовьтесь проверить свои знания в увлекательном мире искусственного интеллекта. В этой викторине вас ждут вопросы по различным темам, включая Машинное Обучение, Глубокое Обучение и Алгоритмы. Независимо от того, являетесь ли вы новичком или энтузиастом искусственного интеллекта, испытайте себя!🧠💡'''
+msg_intr_eng_version = '''🤖Welcome to the Machine learning Quiz Bot!🧠🤖
+
+Get ready to challenge your knowledge in the fascinating world of artificial intelligence. This quiz covers a range of topics, including Machine Learning, Deep Learning, and Algorithms. Whether you're a beginner or an AI enthusiast, let me try you!🧠💡'''
+
+finish_text_rus_version = 'вы завершили текст'
+finish_text_eng_version = 'you finished the test'
+
+help_cmd_eng_version = f'''The test consists of {len(questions)} questions on the topics of "Machine Learning," "Deep Learning," and "Algorithms." At the end of the test, you will receive a message with the results.
+
+The features of the bot are as follows:
+
+📍 Interact using the buttons below each question.
+⌨️ No typing required; just click your answers.
+🌐 Questions presented in English and Russian for your convenience.
+🔄 Feel free to review and change your answers as you go.
+📝 User data recorded to enhance your quiz experience.
+
+Good luck!'''
+help_cmd_rus_version = f'''Тест включает в себя {len(questions)} вопросов на темы: "Машинное обучение", "Глубокое обучение", "Алгоритмы". В конце теста вам будет выведено сообщение с результатами.
+Особенности бота представлены ниже:
+
+ 📍 Взаимодействуйте с помощью кнопок под каждым вопросом.
+⌨ Никакого ввода с клавиатуры, просто кликайте на ваши ответы.
+🌐 Вопросы представлены на английском и русском для вашего удобства.
+🔄 Возможность пересматривать и изменять ответы в процессе.
+📝 Данные пользователя записываются для улучшения вашего опыта викторины.
+
+Удачи!
+'''
